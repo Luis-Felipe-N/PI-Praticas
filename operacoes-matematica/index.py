b8d0aa846fb56c82f84425cb841b2855f45d76d9
luis.nunes <luisj2felipe09@gmail.com>
2023-09-20 18:56:09 -0300

    adicao, subtracao e espelhamento

--- operacoes-matematica/index.py
+++ operacoes-matematica/index.py
@@ -9,21 +9,52 @@ def adicao(imagem_01, imagem_02):
   largura = _largura_01 if _largura_01 > _largura_02 else _largura_02
   altura = _altura_01 if _altura_01 > _altura_02 else _altura_02
 
-  nova_imagem = np.zeros((altura, largura, 3), dtype=np.uint8)
+  nova_imagem = np.zeros((largura, altura, 3), dtype=np.uint8)
 
-  for y in range(altura):
-    for x in range(largura):
-        try:
-          if (x < _largura_01 and y < _altura_01) and (x < _largura_02 and y < _altura_02):
-            print(_largura_01, _altura_01)
-            print(x, y)
-            p1 = imagem_01[y, x]
-            p2 = imagem_02[y, x]
-            nova_imagem[y, x] = (p2 + p1) / 2
-        except:
-          ...
+  for x in range(largura):
+    for y in range(altura):
+      try:
+        if (x < _largura_01 and y < _altura_01) and (x < _largura_02 and y < _altura_02):
+          
+          p1 = imagem_01[x, y]
+          p2 = imagem_02[x, y]
+          nova_imagem[x, y] = (p2 + p1) / 2
+      except:
+        ...
 
   return nova_imagem
+
+def subtracao(imagem_01, imagem_02):
+  _largura_01, _altura_01 = imagem_01.shape[:2]
+  _largura_02, _altura_02 = imagem_02.shape[:2]
+
+  largura = _largura_01 if _largura_01 > _largura_02 else _largura_02
+  altura = _altura_01 if _altura_01 > _altura_02 else _altura_02
+
+  nova_imagem = np.zeros((largura, altura, 3), dtype=np.uint8)
+
+  for x in range(largura):
+    for y in range(altura):
+      try:
+        if (x < _largura_01 and y < _altura_01) and (x < _largura_02 and y < _altura_02):
+          nova_imagem[x, y] = imagem_01[x, y] - imagem_02[x, y]
+      except:
+        ...
+
+  return nova_imagem
+
+def espelhamento(imagem):
+  largura, altura = imagem.shape[:2]
+
+  # Aplique o espelhamento horizontal
+  imagem_espelhada = np.zeros((largura, altura, 3), dtype=np.uint8)
+
+  for x in range(largura):
+      for y in range(altura):
+          pixel = imagem[x, y]
+          imagem_espelhada[largura - x - 1, y] = pixel
+
+  return imagem_espelhada
 
 
 
@@ -31,6 +62,14 @@ def adicao(imagem_01, imagem_02):
 imagem_01 = Image.open('../imagens/img-01.jpg')
 imagem_02 = Image.open('../imagens/img-02.jpg')
 
-adicao_imagem = adicao(np.array(imagem_01), np.array(imagem_02))
-resultado_adicao_imagem = Image.fromarray(adicao_imagem)
-resultado_adicao_imagem.save('imagem_somada.jpg')
+# adicao_imagem = adicao(np.array(imagem_01), np.array(imagem_02))
+# resultado_adicao_imagem = Image.fromarray(adicao_imagem)
+# resultado_adicao_imagem.save('imagem_somada.jpg')
+
+# imagem_subtraida = subtracao(np.array(imagem_01), np.array(imagem_02))
+# resultado_imagem_subtraida = Image.fromarray(imagem_subtraida)
+# resultado_imagem_subtraida.save('imagem_subtraida.jpg')
+
+imagem_espelhamento = espelhamento(np.array(imagem_01))
+resultado_imagem_espelhamento = Image.fromarray(imagem_espelhamento)
+resultado_imagem_espelhamento.save('imagem_espelhamento.jpg')
---
+++ reducao-ampliacao/index.py
@@ -0,0 +1,128 @@
+import numpy as np
+from PIL import Image
+
+
+class Reducao():
+    def vizinho_mais_proximo(self, imagem):
+        # Pegando altura atual
+        altura, largura = imagem.shape[:2]
+        nova_altura, nova_largura = round(altura / 2), round(largura / 2)
+
+        # Criando a matrix da nova imagem
+        nova_imagem = np.zeros(
+            (nova_altura, nova_largura, imagem.shape[2]), dtype=np.uint8)
+        for y in range(nova_altura):
+            for x in range(nova_largura):
+                nova_imagem[y, x] = imagem[y * 2, x * 2]
+
+        return nova_imagem
+
+    def bilinear(self, imagem):
+        # Pegando altura atual
+        altura, largura = imagem.shape[:2]
+
+        # calcula a nova altura e largura da imagem. As novas dimensões têm o dobro do tamanho da imagem original
+        nova_altura, nova_largura = round(altura / 2), round(largura / 2)
+
+        #np.zeros cria um array preenchido com zeros com a forma especificada. O tipo de dados é especificado como np.uint8, o que significa que os valores dos pixels variarão de 0 a 255
+        nova_imagem = np.zeros((nova_altura, nova_largura, imagem.shape[2]), dtype=np.uint8)
+        #iterar sobre cada pixel na nova imagem
+        for y in range(nova_altura):
+            for x in range(nova_largura):
+                #calculam as coordenadas correspondentes na imagem original para cada pixel da nova imagem
+                src_x = x * 2
+                src_y = y * 2
+                #calculam as coordenadas dos quatro pixels da imagem original que serão usadas para calcular o valor do novo pixel
+                x1, y1 = int(np.floor(src_x)), int(np.floor(src_y))
+                x2, y2 = min(x1 + 1, largura - 1), min(y1 + 1, altura - 1)
+                #calculam as distâncias entre a posição mapeada e os pixels originais
+                dx = src_x - x1
+                dy = src_y - y1
+                #recuperam os valores de pixel nas quatro posições calculadas anteriormente
+                pixel1 = imagem[y1, x1]
+                pixel2 = imagem[y1, x2]
+                pixel3 = imagem[y2, x1]
+                pixel4 = imagem[y2, x2]
+                #calcula o novo valor do pixel tomando uma média ponderada dos quatro pixels da imagem original. Os pesos são determinados pelas distâncias (dx e dy) calculadas anteriormente
+                nova_imagem[y, x] = (1 - dx) * (1 - dy) * pixel1 + dx * (1 - dy) * pixel2 + (1 - dx) * dy * pixel3 + dx * dy * pixel4
+                print((1 - dx) * (1 - dy) * pixel1 + dx * (1 - dy) * pixel2 + (1 - dx) * dy * pixel3 + dx * dy * pixel4)
+        return nova_imagem
+
+
+class Ampliacao():
+
+    def vizinho_mais_proximo(self, imagem):
+        altura, largura = imagem.shape[:2]
+        nova_altura, nova_largura = altura * 2, largura * 2
+
+        nova_imagem = np.zeros(
+            (nova_altura, nova_largura, imagem.shape[2]), dtype=np.uint8)
+
+        for y in range(0, nova_altura, 2):
+            for x in range(0, nova_largura, 2):
+                pixel = imagem[y // 2, x // 2]
+                nova_imagem[y, x] = pixel
+                nova_imagem[y + 1, x] = pixel
+                nova_imagem[y, x + 1] = pixel
+                nova_imagem[y + 1, x + 1] = pixel
+
+        return nova_imagem
+
+    def bilinear(self, imagem):
+        altura, largura = imagem.shape[:2]
+        nova_altura, nova_largura = round(altura * 2), round(largura * 2)
+        nova_imagem = np.zeros((nova_altura, nova_largura, imagem.shape[2]), dtype=np.uint8)
+
+        for y in range(nova_altura):
+            for x in range(nova_largura):
+                src_x = x * (largura / nova_largura)
+                src_y = y * (altura / nova_altura)
+
+                x1, y1 = int(np.floor(src_x)), int(np.floor(src_y))
+                x2, y2 = min(x1 + 1, largura - 1), min(y1 + 1, altura - 1)
+
+                dx = src_x - x1
+                dy = src_y - y1
+
+                pixel1 = imagem[y1, x1]
+                pixel2 = imagem[y1, x2]
+                pixel3 = imagem[y2, x1]
+                pixel4 = imagem[y2, x2]
+
+                nova_imagem[y, x] = (1 - dx) * (1 - dy) * pixel1 + dx * \
+                    (1 - dy) * pixel2 + (1 - dx) * \
+                    dy * pixel3 + dx * dy * pixel4
+
+        return nova_imagem
+
+
+# Transformando a imageme em uma matrix
+arquivo_imagem = Image.open('imagens/img-01.jpg')
+matrix_imagem = np.array(arquivo_imagem)
+
+reducao = Reducao()
+ampliar = Ampliacao()
+
+resultado_interpolacao = reducao.bilinear(matrix_imagem)
+resultado_imagem = Image.fromarray(resultado_interpolacao)
+
+resultado = reducao.vizinho_mais_proximo(matrix_imagem)
+resultado_imagem = Image.fromarray(resultado)
+resultado_imagem.save('resultado_reducao_vizinhos.jpg')
+
+# Para redução por vizinhos mais proximo.
+
+resultado = reducao.bilinear(matrix_imagem)
+resultado_imagem = Image.fromarray(resultado)
+resultado_imagem.save('resultado_reducao_bilinear.jpg')
+
+# Para ampliação por vizinhos mais proximo.
+
+resultado = ampliar.vizinho_mais_proximo(matrix_imagem)
+resultado_imagem = Image.fromarray(resultado)
+resultado_imagem.save('resultado_ampliacao_vizinhos.jpg')
+
+# # Para ampliaçao por bilinear
+resultado = ampliar.bilinear(matrix_imagem)
+resultado_imagem = Image.fromarray(resultado)
+resultado_imagem.save('resultado_ampliacao_bilinear.jpg')
